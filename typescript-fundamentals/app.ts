5a7803e930f27f2ebfc294dabdef9cb74577fd6b
iranjith <rgranjith89@gmail.com>
2024-04-13 11:16:35 +0530

    feat: annotating functions with parameters

--- typescript-fundamentals/app.ts
+++ typescript-fundamentals/app.ts
@@ -1,3 +1,20 @@
-let greeting:string="Hello World!";
+function GetReview(title:string): string | number {
+    if (title == "Dune") {
+        return "Excellent";
+    } else {
+        return Math.floor(Math.random() * 10);
+    }
+}
 
-console.log(greeting);
+function PrintMovieInfo(title:string, yearReleased:number=2000, ...cast:string[]){
+    console.log(`Title: ${title}`);
+
+    if(yearReleased){
+        console.log(`Year Released: ${yearReleased}`);
+    }
+
+    console.log(`Cast: ${cast.join(', ')}`);
+}
+
+
+PrintMovieInfo('Dune',2021, 'Timothy','Zendaya');
--- typescript-fundamentals/js/app.js
+++ typescript-fundamentals/js/app.js
@@ -1,3 +1,17 @@
 "use strict";
-let greeting = "Hello World!";
-console.log(greeting);
+function GetReview(title) {
+    if (title == "Dune") {
+        return "Excellent";
+    }
+    else {
+        return Math.floor(Math.random() * 10);
+    }
+}
+function PrintMovieInfo(title, yearReleased = 2000, ...cast) {
+    console.log(`Title: ${title}`);
+    if (yearReleased) {
+        console.log(`Year Released: ${yearReleased}`);
+    }
+    console.log(`Cast: ${cast.join(', ')}`);
+}
+PrintMovieInfo('Dune', 2021, 'Timothy', 'Zendaya');
