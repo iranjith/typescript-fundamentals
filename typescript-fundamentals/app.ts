01ce87460063ca7f6869ebc4b5a05f448cd1f4e7
iranjith <rgranjith89@gmail.com>
2024-04-13 11:54:07 +0530

    feat: function overloading

--- typescript-fundamentals/app.ts
+++ typescript-fundamentals/app.ts
@@ -1,3 +1,18 @@
+function GetAllMovies() {
+    return [
+      { title: 'A New Hope', director: 'George Lucas', yearReleased: 1977, streaming: true },
+      { title: 'The Empire Strikes Back', director: 'Irvin Kershner', yearReleased: 1980, streaming: false },
+      { title: 'Return of the Jedi', director: 'Richard Marquand', yearReleased: 1983, streaming: true },
+      { title: 'The Phantom Menace', director: 'George Lucas', yearReleased: 1999, streaming: false },
+      { title: 'Attack of the Clones', director: 'George Lucas', yearReleased: 2002, streaming: true },
+      { title: 'Revenge of the Sith', director: 'George Lucas', yearReleased: 2005, streaming: true },
+      { title: 'The Force Awakens', director: 'J.J. Abrams', yearReleased: 2015, streaming: false },
+      { title: 'The Last Jedi', director: 'Rian Johnson', yearReleased: 2017, streaming: true },
+      { title: 'The Rise of Skywalker', director: 'J.J. Abrams', yearReleased: 2019, streaming: true }
+    ];
+  }
+  
+
 function GetReview(title:string): string | number {
     if (title == "Dune") {
         return "Excellent";
@@ -17,7 +32,33 @@ function PrintMovieInfo(title:string, yearReleased:number=2000, ...cast:string[]
 }
 
 
+function GetTitles(director: string): string[];
+function GetTitles(director: string, streaming: boolean): string[];
+function GetTitles(director: string, streaming?: boolean): string[] {
+  const allMovies = GetAllMovies();
+  const searchResults: string[] = [];
+
+  if(streaming !== undefined) {
+    for(let movie of allMovies) {
+      if(movie.director === director && movie.streaming === streaming) {
+        searchResults.push(movie.title);
+      }
+    }
+  } else {
+    for(let movie of allMovies) {
+      if(movie.director === director) {
+        searchResults.push(movie.title);
+      }
+    }
+  }
+  return searchResults;    
+}
+
+let movies:string[] = GetTitles('George Lucas', false);
+movies.forEach(title => console.log(title));
+
+
 //PrintMovieInfo('Dune',2021, 'Timothy','Zendaya');
 
-const  LogMessage = (message:string) =>  console.log(message);
-LogMessage("Hello World"); 
+// const  LogMessage = (message:string) =>  console.log(message);
+// LogMessage("Hello World"); 
--- typescript-fundamentals/js/app.js
+++ typescript-fundamentals/js/app.js
@@ -1,4 +1,17 @@
 "use strict";
+function GetAllMovies() {
+    return [
+        { title: 'A New Hope', director: 'George Lucas', yearReleased: 1977, streaming: true },
+        { title: 'The Empire Strikes Back', director: 'Irvin Kershner', yearReleased: 1980, streaming: false },
+        { title: 'Return of the Jedi', director: 'Richard Marquand', yearReleased: 1983, streaming: true },
+        { title: 'The Phantom Menace', director: 'George Lucas', yearReleased: 1999, streaming: false },
+        { title: 'Attack of the Clones', director: 'George Lucas', yearReleased: 2002, streaming: true },
+        { title: 'Revenge of the Sith', director: 'George Lucas', yearReleased: 2005, streaming: true },
+        { title: 'The Force Awakens', director: 'J.J. Abrams', yearReleased: 2015, streaming: false },
+        { title: 'The Last Jedi', director: 'Rian Johnson', yearReleased: 2017, streaming: true },
+        { title: 'The Rise of Skywalker', director: 'J.J. Abrams', yearReleased: 2019, streaming: true }
+    ];
+}
 function GetReview(title) {
     if (title == "Dune") {
         return "Excellent";
@@ -14,6 +27,27 @@ function PrintMovieInfo(title, yearReleased = 2000, ...cast) {
     }
     console.log(`Cast: ${cast.join(', ')}`);
 }
+function GetTitles(director, streaming) {
+    const allMovies = GetAllMovies();
+    const searchResults = [];
+    if (streaming !== undefined) {
+        for (let movie of allMovies) {
+            if (movie.director === director && movie.streaming === streaming) {
+                searchResults.push(movie.title);
+            }
+        }
+    }
+    else {
+        for (let movie of allMovies) {
+            if (movie.director === director) {
+                searchResults.push(movie.title);
+            }
+        }
+    }
+    return searchResults;
+}
+let movies = GetTitles('George Lucas', false);
+movies.forEach(title => console.log(title));
 //PrintMovieInfo('Dune',2021, 'Timothy','Zendaya');
-const LogMessage = (message) => console.log(message);
-LogMessage("Hello World");
+// const  LogMessage = (message:string) =>  console.log(message);
+// LogMessage("Hello World"); 
